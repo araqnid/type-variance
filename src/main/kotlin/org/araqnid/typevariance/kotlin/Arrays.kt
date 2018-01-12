187366e789d3dc929b4695aba5aeaa5307a0e896
Steve Haslam <araqnid@googlemail.com>
2018-01-12 19:52:42 +0000

    Some commentary on what's going on in the Kotlin Arrays version

--- src/main/kotlin/org/araqnid/typevariance/kotlin/Arrays.kt
+++ src/main/kotlin/org/araqnid/typevariance/kotlin/Arrays.kt
@@ -12,12 +12,16 @@ fun main(args: Array<String>) {
 }
 
 fun copy(from: Array<out Any>, to: Array<Any>) {
+    // l-value from[0] is forbidden to access
+    // (as if type of input use site was Nothing)
     assert(from.size == to.size)
     for (i in from.indices)
         to[i] = from[i]
 }
 
 fun <T> fill(dest: Array<in T>, value: T) {
+    // type of dest[0] is `Any?`, not `T`
+    // i.e. output use sites of Array are now type Any?
     for (i in dest.indices)
         dest[i] = value
 }
