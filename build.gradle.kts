f014abff6693a24f78d35a36f54089edf98d3a7b
Steve Haslam <araqnid@googlemail.com>
2019-02-16 20:02:24 +0000

    Gradle 5.2, Scala 2.12.8, Kotlin 1.3.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    kotlin("jvm") version "1.2.30"
+    kotlin("jvm") version "1.3.21"
     scala
+    groovy
 }
 
 repositories {
@@ -18,14 +19,41 @@ tasks {
     }
 
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions {
-            jvmTarget = "1.8"
-        }
+        sourceCompatibility = "1.8"
+        targetCompatibility = "1.8"
+        kotlinOptions.jvmTarget = "1.8"
+    }
+
+    withType<GroovyCompile> {
+        sourceCompatibility = "1.8"
+        targetCompatibility = "1.8"
+        groovyOptions.isVerbose = true
+        options.encoding = "UTF-8"
+        options.compilerArgs.add("-parameters")
+        options.isIncremental = true
+        options.isDeprecation = true
+    }
+
+    withType<ScalaCompile> {
+        sourceCompatibility = "1.8"
+        targetCompatibility = "1.8"
+        scalaCompileOptions.encoding = "UTF-8"
+        scalaCompileOptions.isDeprecation = true
+        scalaCompileOptions.additionalParameters = listOf("-feature")
+        options.encoding = "UTF-8"
+        options.isIncremental = true
+        options.isDeprecation = true
     }
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("reflect"))
-    implementation("org.scala-lang:scala-library:2.12.4")
+    implementation("org.scala-lang:scala-library:2.12.8")
+    implementation(localGroovy())
+
+    testImplementation("junit:junit:4.12")
+    testImplementation("org.hamcrest:hamcrest-library:1.3")
+    testImplementation("com.natpryce:hamkrest:1.7.0.0")
+    testImplementation("org.scalatest:scalatest_2.12:3.0.5")
 }
